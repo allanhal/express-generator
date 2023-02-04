641f753c0cc34935a6942cceef66e3f1c47380d8
sarah <sarah@gmail.com>
2023-02-04 16:34:01 -0300

    read file and delete node_modules

--- routes/filter.js
+++ routes/filter.js
@@ -1,16 +1,21 @@
 var express = require("express");
 var router = express.Router();
+const fs = require("fs");
 
-const produtos = require('../dados/produtos.json')
-  
+// const produtos = require('../dados/produtos.json')
 
 router.get("/", (req, res, next) => {
-  const termo = req.query.q;
-  res.send(
-    produtos.filter((produto) =>
-      produto.titulo.toLowerCase().includes(termo.toLowerCase())
-    )
-  );
+  fs.readFile("./dados/produtos.json",(err, data) => {
+    if (err) throw err;
+
+    const produtos = JSON.parse(data);
+    const termo = req.query.q;
+    res.send(
+      produtos.filter((produto) =>
+        produto.titulo.toLowerCase().includes(termo.toLowerCase())
+      )
+    );
+  });
 });
 
 module.exports = router;
